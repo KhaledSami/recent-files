7a73f5d334f420b42373eb86f07a39b42e774e51
khaledSami <k.sami.mohammed@gmail.com>
2015-01-15 04:42:49 +0100

    Adding the integration test suite

---
+++ test/test-integration.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var assert = require('assert');
+var chai = require('chai');
+var sinon = require('sinon');
+var fs = require('fs');
+
+var directoryWatcher = require('../src/directory-watcher.js').directoryWatcher;
+var RecentFilesHandler = require('../src/recent-files.js').RecentFilesHandler;
+
+
+suite('Integration between DirectoryWatcher and RecentFilesHandler', function() {
+
+    setup(function() {
+        if (!fs.existsSync('.testFiles')) {
+            fs.mkdirSync(".testFiles");
+            fs.writeFileSync(".testFiles/a", "abc");
+            fs.writeFileSync(".testFiles/b", "");
+        }
+    });
+    test('file Watcher will record changes on watched Directory', function(done) {
+
+        var recentFilesHandler = new RecentFilesHandler(3);
+
+        var watcher = directoryWatcher.startListen('.testFiles', recentFilesHandler);
+
+        fs.appendFileSync(".testFiles/b", 'abc');
+        fs.writeFileSync(".testFiles/c", "");
+
+        directoryWatcher.recordChanges(watcher, recentFilesHandler, function() {
+            if (recentFilesHandler.fileList.length === 3) {
+                assert.deepEqual(['c', 'b', 'a'], recentFilesHandler.fileList);
+                directoryWatcher.stopListen(watcher);
+                done();
+            }
+        });
+
+    });
+    
+    teardown(function() {
+        fs.unlinkSync(".testFiles/a");
+        fs.unlinkSync('.testFiles/b');
+        fs.unlinkSync('.testFiles/c');
+        fs.rmdirSync(".testFiles");
+    });
+});
